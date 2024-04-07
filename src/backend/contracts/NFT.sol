3de309fd9189ab961dcf5f4f1c8795dfe15deb8f
Yihang H <huangyihang1992@gmail.com>
2024-04-07 01:07:35 -0400

    template

---
+++ src/backend/contracts/NFT.sol
@@ -0,0 +1,18 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.4;
+
+import "@openzeppelin/contracts/token/ERC721/extensions/ERC721URIStorage.sol";
+
+/// inherit ERC721URIStorage to store tokenURI
+contract NFT is ERC721URIStorage {
+    uint public tokenCount;
+    constructor() ERC721("DApp NFT", "DAPP"){}
+
+    /// memory: the address of the input string
+    function mint(string memory _tokenURI) external returns(uint) {
+        tokenCount ++;
+        _safeMint(msg.sender, tokenCount);
+        _setTokenURI(tokenCount, _tokenURI);
+        return(tokenCount);
+    }
+}
